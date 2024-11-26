f82de93d7da98d036995e749bb20448afb293a9b
Isabel Guan <iguan@usc.edu>
2024-11-26 01:05:56 -0800

    Harvester updates

--- Protect the Cube/Assets/Scripts/Harvester.cs
+++ Protect the Cube/Assets/Scripts/Harvester.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class Harvester : Building
 {
     public GameObject currentPlaceableObject;
     [SerializeField] public float radius = 4.0f;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     bool IsValidPlacement(float radius = 4.0f)
     {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);//, -1, QueryTriggerInteraction.Collide);
@@ -16,18 +23,31 @@ public class Harvester : Building
                 continue;
             }
             GetComponent<RangeIndicator>().ShowIndicator();
-            if (other.gameObject.GetComponent<Harvester>() != null)
+
+            if (other.gameObject.GetComponent<Harvester>() != null || other.transform.root.gameObject.GetComponent<Harvester>() != null)
             {
                 // other.gameObject.GetComponent<RangeIndicator>().ShowIndicator();
                 Debug.Log("Invalid Placement: too close to existing harvester");
+                anim.SetTrigger("Damage");
                 return false;
             } else if (other.gameObject.GetComponent<turretShoot>() != null)
             {
                 // other.gameObject.GetComponent<RangeIndicator>().ShowIndicator();
+                anim.SetTrigger("Damage");
                 Debug.Log("Invalid Placement: too close to turret");
                 return false;
             } 
         }
+
+        // turn off animation for top and bottom
+        if (anim != null)
+        {
+            anim.enabled = false; // Disable the Animator
+            anim.Play("New State", -1, 0f); // Reset to the Idle state
+            anim.Update(0); // Force an update to apply changes
+            anim.enabled = true; // Re-enable the Animator
+        }
+
         return true;
     }
 
